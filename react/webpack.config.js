c0c9e267964b8f9222d2e4df2093c581490968d7
Ben <544652471@qq.com>
2016-09-04 16:55:49 +0800

    修改图片路径

--- react/webpack.config.js
+++ react/webpack.config.js
@@ -1,22 +1,21 @@
 
 'use strict';
 
-var path = require('path');
-var webpack = require('webpack');
+var webpack = require('webpack');    // 还是 es5 的写法
 
-module.exports = {
+module.exports = {                   // webpack 的配置项写在这里面
     entry: [
-        "./app/entry.js"
+        './app/entry.js'             // 项目的入口文件，可以有多个！！！
     ],
     output: {
-        path: './build/',
+        path: './build/',           // 最终输出文件路径
         filename: 'bundle.js'
     },
     module: {
         loaders: [
-            {test: /\.(js|jsx)$/, loader: "jsx-loader?harmony"},
-            {test: /\.css$/, loader:"style-loader!css-loader"},
-            {test: /\.(jpg|png)$/, loader: "url-loader"}
+            {test: /\.(js|jsx)$/, loader: 'jsx-loader?harmony'},
+            {test: /\.css$/, loader:'style-loader!css-loader'},
+            {test: /\.(jpg|png)$/, loader: 'url?limit=10000'}
         ]
     }
 };
