0aa3740af98c1144034e0793b66301310deb9c8a
xiazhigang <xiazhigang@qiushibaike.com>
2017-04-23 08:29:33 +0800

    feat: 终于爬取到第一页的数据并存储到 mongodb 了。

--- spider/index.js
+++ spider/index.js
@@ -2,40 +2,26 @@ var cheerio = require('cheerio')
 var request = require('superagent')
 var fs = require('fs')
 
-var url = 'http://api.shouqu.me/api_service/api/v1/mark/webList'
-var html = ''
-
-// request.get(url).end((err, res)=>{
-//     console.log(handleDB(res.text));
-// });
-
-function handleDB(html) {
-    var $ = cheerio.load(html)
-    var info = $('#info')
-
-    // 获取电影名(成功拿到了)
-    var movieName = $('#content>h1>span').filter(function(i, el) {
-        return $(this).attr('property') === 'v:itemreviewed';
-    }).text();
-
-    // 获取影片导演名
-     var directories = '- 导演：' + $('#info span a').filter(function(i,el){
-       return $(this).attr('rel') === 'v:directedBy';
-     }).text();
-
-     return directories
+var url1 = 'https://api.shouqu.me/api_service/api/v1/mark/webList'
+var data = {
+  userId:223558,
+  lastupdataTime:1492873515827,
+  pageNo:1,
+  pageSize:30,
+  sort:'desc',
+  renderType:0
 }
+
+var browserMsg={
+    "User-Agent":"Mozilla/5.0 (Windows NT 6.3; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/48.0.2564.116 Safari/537.36",
+    'Content-Type':'application/x-www-form-urlencoded; charset=UTF-8'
+};
 
 // 测试连接 mongodb
 var mongoose = require('mongoose');
 //重点在这一句，赋值一个全局Promise
 mongoose.Promise = global.Promise;
 var db = mongoose.connect('mongodb://192.168.31.205:27017/shouqu');
-
-// db.on('error', console.error.bind(console, '连接错误'));
-// db.open('once', function(){
-//     console.log('连接成功');
-// })
 
 // 创建schema
 const indexSchema = new mongoose.Schema({
@@ -48,25 +34,38 @@ const indexSchema = new mongoose.Schema({
 
 const col = mongoose.model('col', indexSchema)
 
-const test2 = {
-    title: 'test2',
-    url: 'www.baidu.com',
+const data1 = {
+    title: '',
+    url: '',
     channel: 1,
     channelName: '收趣云书签',
-    sourceName: '推酷'
+    sourceName: ''
 }
 
-//初始化model
-var insert = new col(test2);
-
-//执行插入
-insert.save(function(err, result){
-  if(err){
-    console.log(err);
-  }else{
-    console.log('insert ok');
-  }
-
-  //关闭连接
-  // db.close();
-});
+request
+  .post(url1)
+  .set(browserMsg)
+  .send(data)
+  .end(function(err, res){
+    // console.log(res)
+    if (err || !res.ok) {
+      console.log(err);
+    } else {
+      console.log(typeof(res.body))
+      var list = res.body.data.list
+      for( index in list ){
+        console.log(list[index])
+        data1.title = list[index].title
+        data1.url = list[index].url
+        data1.sourceName = list[index].sourceName
+        //初始化model
+        var insert = new col(data1);
+        insert.save(function(err, result){
+          if(err){
+            console.log(err)
+            return false
+          }
+        })
+      }
+    }
+  })
