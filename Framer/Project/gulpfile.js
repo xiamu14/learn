907ceb6060087d0d75e22234f949d284f3a4d5a8
xiazhigang <544652471@qq.com>
2017-06-06 07:15:19 +0800

    feat: 清理不要的文件夹

--- Framer/Project/gulpfile.js
+++
@@ -1,8 +0,0 @@
-var gulp = require("gulp");
-var coffee = require("gulp-coffee");
-
-gulp.task('coffee', function() {
-  gulp.src('./src/*.coffee')
-    .pipe(coffee({bare: true}))
-    .pipe(gulp.dest('./dest/'));
-});
