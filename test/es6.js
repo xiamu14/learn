a746465a615948ef8a31ea6c1477c9f3ad6d2782
xiazhigang <544652471@qq.com>
2017-06-29 17:45:49 +0800

    feat: 测试 es6 的东西

--- test/es6.js
+++ test/es6.js
@@ -1,12 +1,12 @@
 var obj = new Proxy({}, {
-  get: function (target, key, receiver) {
-    console.log(`getting ${key}!`);
-    return Reflect.get(target, key, receiver);
-  },
-  set: function (target, key, value, receiver) {
-    console.log(`setting ${key}!`);
-    return Reflect.set(target, key, value, receiver);
-  }
+    get: function(target, key, receiver) {
+        console.log(`getting ${key}!`);
+        return Reflect.get(target, key, receiver);
+    },
+    set: function(target, key, value, receiver) {
+        console.log(`setting ${key}!`);
+        return Reflect.set(target, key, value, receiver);
+    }
 });
 
 obj.count = 1
@@ -16,16 +16,26 @@ obj.count = 1
 // 2017年6月10日
 //
 function Person(name, age, gender) {
-  this.name = name
-  this.age = age
-  this.gender = gender
+    this.name = name
+    this.age = age
+    this.gender = gender
 }
 
 var first = new Person('lisi', 12, 'male')
 var second = new Person('li', 23, 'female')
 
-Person.prototype.getName = function(){
-  return this.name
-}
-
+Person.prototype.getName = function() {
+        return this.name
+    }
+    //
 console.log(second.getName())
+
+// 2017年6月29日
+//
+// 解构赋值
+//
+let x = 1,
+    y = 2;
+
+[x, y] = [y, x]
+console.log(x);
