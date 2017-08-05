e6e2d7f0c01642050c88700b76c10cf64eb78058
xiazhigang <544652471@qq.com>
2017-08-06 00:37:59 +0800

    feat: import nyc

--- monkey/gulp_tasks/css.js
+++ monkey/gulp_tasks/css.js
@@ -1,10 +1,11 @@
 import cssnext from 'postcss-cssnext'
+import cssnano from 'cssnano'
 
 export function css(gulp, $, entry) {
     return function() {
         gulp.src(entry)
             .pipe($.debug())
-            .pipe($.postcss([cssnext()]))
+            .pipe($.postcss([cssnext(), cssnano()]))
             .pipe(gulp.dest('./static/dist'))
             .on('end', () => {
                 $.util.log(' 编译完成')
