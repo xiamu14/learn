3542bc06613cf9e2b4f9146af4076c2b1d04ef0c
xiazhigang <544652471@qq.com>
2017-09-16 23:26:10 +0800

    learn python

---
+++ python-tutorial/leason01.py
@@ -0,0 +1,6 @@
+# -*- coding: utf-8 -*-
+print '你好'
+print 'hello again'
+print "Yay! Printing"
+print "I'd much rather you 'not'"
+print 'I "said" do not touch this'
---
+++ python-tutorial/leason02.py
@@ -0,0 +1,5 @@
+# A comment, this is so you can read your program later
+# Anything after the # is ignored by python
+
+print "I could have code like this." # and the comment after is ignored
+
