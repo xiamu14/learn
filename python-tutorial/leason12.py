4f0488a3d01dba7a4700f93cbfc4797cfdde7df5
xiazhigang <544652471@qq.com>
2017-09-19 01:17:26 +0800

    test.

--- python-tutorial/leason12.py
+++ python-tutorial/leason12.py
@@ -5,14 +5,18 @@ from sys import argv
 
 script, filename = argv
 
-print "we're going to erase %r" % filename
-print "If you don't want that, hit CTRL-C(^C)"
-print "If you do want that, hit RETURN."
+print("we're going to erase %r" % filename)
+print("If you don't want that, hit CTRL-C(^C)")
+print("If you do want that, hit RETURN.")
 
-raw_input("?")
+input("?")
 
-print "Opening the file……"
+print("Opening the file……")
 
 target = open(filename, 'w')
 
-print "Truncating the file. Goodbye!"
+print("Truncating the file. Goodbye!")
+
+target.write('line 1: hello world')
+
+target.close()
