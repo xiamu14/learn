cfd1b6c39f28a73b51a176920ad48a96eb8adad2
xiazhigang <544652471@qq.com>
2017-09-19 07:57:48 +0800

    笨方法学习 python~14

---
+++ python-tutorial/leason14.py
@@ -0,0 +1,17 @@
+# this one is like you scripts with argv
+
+def print_two(*args):
+    arg1, arg2 = args
+    print("arg1: %r, arg2:%r"%(arg1, arg2))
+
+# ok, that *args is actually pointless, we can just do this
+def print_two_again(arg1, arg2):
+    print("arg1:%r,arg2:%r"%(arg1, arg2))
+
+# this just takes one argument
+def print_none():
+    print("I got nothin.")
+
+print_none()
+print_two('Zed', "Shaw")
+print_two_again("zed", "shaw")
