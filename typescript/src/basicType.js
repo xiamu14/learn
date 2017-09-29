68be84828cd7f8e0632e1b51d76ae7d908308bc1
xiazhigang <544652471@qq.com>
2017-09-29 08:20:42 +0800

    tscript 基本类型练习

---
+++ typescript/src/basicType.js
@@ -0,0 +1,20 @@
+// boolean
+var b = false;
+console.log(b);
+// Number
+var n = 10;
+var n1 = 0x15; // 21
+var n2 = 5; // 5
+var n3 = 62; // 62
+console.log(n, n1, n2, n3);
+// String
+var color = 'red';
+color = "blue";
+// Array
+var array = [12, 2];
+// Tuple
+var array1 = ['233', 312];
+// void
+function warnUser() {
+    console.log('世界美好。');
+}
---
+++ typescript/src/basicType.ts
@@ -0,0 +1,25 @@
+// boolean
+let b : boolean = false;
+console.log(b);
+
+// Number
+let n : number = 10;
+let n1 : number = 0x15;  // 21
+let n2 : number = 0b0101; // 5
+let n3 : number = 0o76; // 62
+console.log(n, n1, n2, n3);
+
+// String
+let color : string = 'red';
+color = "blue";
+
+// Array
+let array : number[] = [12, 2];
+
+// Tuple
+let array1 : [string, number] = ['233', 312];
+
+// void
+function warnUser(): void {
+    console.log('世界美好。')
+}
