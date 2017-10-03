6b921bc5a401b816e0b7c41d12d4d2efc71ada5c
xiazhigang <544652471@qq.com>
2017-10-04 00:31:44 +0800

    graphQL 第5课完结。

--- graphQL/src/schema/index.js
+++ graphQL/src/schema/index.js
@@ -1,52 +1,57 @@
+import axios from "axios";
 import {
-    GraphQLObjectType,
-    GraphQLSchema,
-    GraphQLString,
-    GraphQLList,
-    GraphQLInt
-} from 'graphql'
+  GraphQLObjectType,
+  GraphQLSchema,
+  GraphQLString,
+  GraphQLList,
+  GraphQLInt
+} from "graphql";
 
 // 我们要用的模拟数据
-const data = require('../../data.json')
+const data = require("../../data.json");
 
 const User = new GraphQLObjectType({
-    name: 'User',
-    description: 'User对象',
-    fields: {
-        id: {
-            type: GraphQLInt
-        },
-        name: {
-            type: GraphQLString
-        },
+  name: "User",
+  description: "User对象",
+  fields: {
+    id: {
+      type: GraphQLInt
+    },
+    name: {
+      type: GraphQLString
     }
+  }
 });
 
 const Query = new GraphQLObjectType({
-    name: 'Query',
-    fields: {
-        user: {
-            type: User,
-            args: {
-                id: {
-                    type: GraphQLInt
-                }
-            },
-            resolve(_, args) {
-                return data[args.id];
-            }
-        },
-        users: {
-            type: new GraphQLList(User),
-            resolve(_, args) {
-                return data
-            }
+  name: "Query",
+  fields: {
+    user: {
+      type: User,
+      args: {
+        id: {
+          type: GraphQLInt
         }
+      },
+      resolve(_, args) {
+        // return data["users"][args.id];
+        return axios
+          .get(`http://localhost:3000/users/${args.id}`)
+          .then(res => res.data);
+      }
+    },
+    users: {
+      type: new GraphQLList(User),
+      resolve(_, args) {
+        // return data["users"]
+        return axios.get(`http://localhost:3000/users`).then(res => res.data);
+      }
     }
+  }
 });
 
 const Schema = new GraphQLSchema({
-    query: Query
+  query: Query
 });
 
 export default Schema;
