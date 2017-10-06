a95150617a68c031cf81261d53e1ae0636ff14b0
xiazhigang <544652471@qq.com>
2017-10-07 00:08:11 +0800

    php_tutorial && swift_tutorial

--- ios_tutorial/MyPlayground.playground/Contents.swift
+++ ios_tutorial/MyPlayground.playground/Contents.swift
@@ -2,67 +2,14 @@
 
 import UIKit
 
-let capacity = 30
-let volume = 50
-let price = 10
-let money = 20
+// String.index
 
-// 三目运算符
-var battery = 21
-var batteryColor:UIColor
+var str = "Hello, Swift"
+let startIndex = str.startIndex
+print(str[startIndex])
 
-// 区间
-for index in 0..<10 {
-    index
-}
+let s = "one third is \(1.0/3.0)"
 
-// switch
+// NSString
 
-let val = 1
-
-switch val {
-case  1:
-    print("h")
-default:
-    ()
-}
-
-// switch 高级用法
-let score = 90
-
-switch score {
-case 1...60:
-    print("C")
-case 61...80:
-    print("B")
-case 81...100:
-    print("A")
-default:
-    break;
-}
-
-// x^4 - y^2 = 15*x*y
-
-findAnswer:for m in 1...300 {
-    for n in 1...300 {
-        if m*m*m*m - n*n == 15*m*n{
-            print(m,n)
-            break findAnswer
-        }
-    }
-}
-
-// case ...where
-for case let i in 0...100 where i % 3 == 0 {
-    print(i)
-}
-
-//guard
-func buy(money: Int, price: Int, capacity: Int, valume: Int){
-    if money >= price {
-        if capacity >= volume {
-            print("I can buy it!")
-            print("\(money-price)")
-        }
-    }
-}
+let s2 = NSString(format:"one third is %.2f", 1.0/3.0) as String
