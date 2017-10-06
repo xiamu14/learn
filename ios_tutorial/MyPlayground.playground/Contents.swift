1c37503d0d526bf26d07e7e84e92891be275a138
xiazhigang <544652471@qq.com>
2017-10-06 21:17:12 +0800

    swift_tutorial

---
+++ ios_tutorial/MyPlayground.playground/Contents.swift
@@ -0,0 +1,16 @@
+//: Playground - noun: a place where people can play
+
+import UIKit
+
+let capacity = 30
+let volume = 50
+let price = 10
+let money = 20
+
+// 三目运算符
+var battery = 21
+var batteryColor:UIColor
+
+
+
+
