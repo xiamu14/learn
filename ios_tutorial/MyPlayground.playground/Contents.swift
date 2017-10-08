c9a38255b19db0bffb9862a07d21f2f531db2676
xiazhigang <544652471@qq.com>
2017-10-08 10:23:28 +0800

    swift-tutorial

--- ios_tutorial/MyPlayground.playground/Contents.swift
+++ ios_tutorial/MyPlayground.playground/Contents.swift
@@ -1,3 +1,60 @@
 //: Playground - noun: a place where people can play
 
 import UIKit
+
+// 枚举的原始值，rawValue
+enum Month: Int{
+    case January = 1
+    case February = 2
+    case March = 3
+    case April = 4
+    case May = 5
+    case June = 6
+    case July = 7
+    case August = 8
+    case September = 9
+    case October = 10
+    case November = 11
+    case December = 12
+}
+
+let curMonth = Month.October
+
+func season( month:Month ) -> String{
+    switch month {
+    case .March,.April,.May:
+        return "Spring"
+    case .June, .July, .August:
+        return "Summer"
+    case .September, .October, .November:
+        return "Autumn"
+    case .December, .January, .February:
+        return "Winter"
+    }
+}
+
+func monthsBeforeNewYear(month:Month) -> Int{
+    return 12 - month.rawValue
+}
+
+print(season(month:curMonth))
+print(monthsBeforeNewYear(month: curMonth))
+
+// Associate Value
+enum ATMStatus{
+    case Success(Int)
+    case Error(String)
+}
+
+var balance = 1000
+
+func withdraw(amount:Int)-> ATMStatus{
+    if balance >= amount{
+        balance -= amount
+        return .Success(balance)
+    }else {
+        return .Error("Not Enough Money")
+    }
+}
+
+withdraw(amount: 10000)
