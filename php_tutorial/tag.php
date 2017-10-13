c84138e0e2c433d82df542e3ad557f13ff38d129
xiazhigang <544652471@qq.com>
2017-10-13 08:23:58 +0800

    test.

---
+++ php_tutorial/tag.php
@@ -0,0 +1,13 @@
+<?php
+    $bool0 = FALSE;
+    $bool1 = TRUE;
+    $bool0 = 1;
+    echo $bool0;
+    var_dump((bool) "1");
+    echo PHP_INT_MAX;
+    echo PHP_INT_MIN;
+    $str = "hanmeimei";
+    echo (int) ( (0.1+0.7) * 10 );
+    echo "Hello, world. $str".PHP_EOL;
+    $array = [0, 1, 3, 4 ,5];
+    var_dump($array);
