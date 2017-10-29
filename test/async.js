d14204af4040e8bdbe5bb3ee4c7e5e7310d37195
xiazhigang <544652471@qq.com>
2017-10-29 16:59:47 +0800

    feat: async await 学习练习。。。

--- test/async.js
+++ test/async.js
@@ -1,21 +1,26 @@
-function resolveAfterTwoSecend(x) {
-  return new Promise((resolve) => {
+function res1(time) {
+  return new Promise((resolve, reject) => {
     setTimeout(()=>{
-      resolve(x);
-    } , 2000)
-  });
+      resolve(10);
+    }, time * 1000);
+  })
 }
 
-async function add() {
-  const a = await resolveAfterTwoSecend(20);
-  const b = await resolveAfterTwoSecend(40);
-  return a + b;
+function res2(time, res) {
+  return new Promise((resolve, reject) => {
+    setTimeout(()=>{
+      resolve(20 + res);
+    }, time * 1000);
+  })
 }
 
-add().then(v => {
-  console.log(v);
-});
+async function test() {
+  const a = await res1(1);
+  const b = await res2(3, 10);
 
-// test
+  const c = await Promise.all([a, b]);
 
-console.log(async ()=>{});
+  console.log(c);
+}
+
+test();
