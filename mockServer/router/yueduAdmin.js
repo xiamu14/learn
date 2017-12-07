c4e006278afb4ddf05cfd788d774a5d700c4c398
xiazhigang <544652471@qq.com>
2017-12-07 20:16:30 +0800

    test.

--- mockServer/router/yueduAdmin.js
+++ mockServer/router/yueduAdmin.js
@@ -4,7 +4,25 @@ const yueduAdminData = require('../data/yueduAdminData');
 const router = express.Router();
 
 router.get('/user', (req, res) => {
-  res.json(yueduAdminData.userTable);
+  if(req.query.page){
+    res.json(yueduAdminData.userTable);
+  }
+  if(req.query.u) {
+    res.json(yueduAdminData.userDetail);
+  }
 });
-
+router.get('/count', (req, res) => {
+  if(req.query.p) {
+    if(req.query.cate === 'list'){
+      // 导出数据接口
+      res.json(yueduAdminData.channelDataEx);
+    } else {
+      // 渠道信息显示
+      res.json(yueduAdminData.channelData);
+    }
+  } else {
+    // 注册统计信息显示
+    res.json(yueduAdminData.accountData);
+  }
+})
 module.exports = router;
