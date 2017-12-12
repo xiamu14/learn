3e80db64e0bb09d64e1fceaef43805d19f6e68b2
xiazhigang <544652471@qq.com>
2017-12-12 15:04:30 +0800

    feat: 添加 promise 的实践

---
+++ promise/index.js
@@ -0,0 +1,39 @@
+function sleep(timer, state) {
+  return new Promise(((reslove) => {
+    setTimeout(() => {
+      // things
+      console.log('sleep:返回 OK');
+      reslove('sleep:ok');
+      if (state === 404) {
+        throw new Error('sleep:这里有个 404');
+      }
+    }, timer);
+  }));
+}
+
+function lazy(timer, state) {
+  return new Promise(((reslove) => {
+    setTimeout(() => {
+      // things
+      console.log('lazy:返回 OK');
+      reslove('lazy:ok');
+      if (state === 404) {
+        throw new Error('lazy:这里有个 404');
+      }
+    }, timer);
+  }));
+}
+
+async function start() {
+  console.log('等待 3秒>>>>');
+  const sleepRes = sleep(3000, 403);
+  const lazyRes = lazy(4000, 403);
+  try {
+    const result = await Promise.all([sleepRes, lazyRes]);
+    console.log(`得到回馈${result}结束>>>>`);
+  } catch (err) {
+    console.log(err);
+  }
+}
+
+start();
