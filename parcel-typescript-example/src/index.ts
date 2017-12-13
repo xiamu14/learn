61a63d6fee0cea2dc9875b92df6d8c71d6eb0d71
xiazhigang <544652471@qq.com>
2017-12-13 17:22:45 +0800

    feat: 修改下测试

--- parcel-typescript-example/src/index.ts
+++ parcel-typescript-example/src/index.ts
@@ -4,22 +4,6 @@ const hello = () => {
 // main
 const main = document.getElementById("js-main");
 if (main) {
-    main.textContent = hello();
+    main.textContent = navigator.userAgent;
 }
 
-// 布尔值
-let isDone: boolean = true;
-// 数字
-let decLiteral: number = 6;
-let hexLiteral: number = 0xf00d;
-let binaryLiteral: number = 0b1010;
-let octalLiteral: number = 0o744;
-
-//字符串
-let myname: string = `Gene`;
-let age: number = 37;
-let sentence: string = `Hello, my myname is ${myname}.
-
-I'll be ${ age + 1} years old next month.`;
-
-console.log(sentence);
