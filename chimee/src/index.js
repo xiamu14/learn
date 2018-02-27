682e30fa06868f03f79b418cbd69162f8dba04a5
xiazhigang <xiazhigang@qiushibaike.com>
2018-02-27 18:56:28 +0800

    test: 测试 chimee 播放插件

--- chimee/src/index.js
+++ chimee/src/index.js
@@ -1,20 +1,8 @@
-// import ChimeeMobilePlayer from "chimee-mobile-player";
+import ChimeePlayer from 'chimee-player';
 
-// new ChimeeMobilePlayer({
-//   wrapper: '#wrapper',  // video dom容器
-//   src: 'http://chimee.org/vod/1.mp4',
-//   autoplay: true,
-//   controls: true,
-//   playsInline: true,
-//   preload: true,
-//   x5VideoPlayerFullscreen: true,
-//   x5VideoOrientation: true,
-//   xWebkitAirplay: true,
-//   muted: true
-// });
-
-import Chimee from 'chimee';
-const chimee = new Chimee('#wrapper');
-chimee.on('play', () => console.log('play!!'));
-chimee.load('http://cdn.toxicjohann.com/lostStar.mp4');
-chimee.play(); // play!!
+const chimee = new ChimeePlayer({
+    wrapper: '#wrapper',
+    src: 'http://cdn.toxicjohann.com/lostStar.mp4',
+    controls: true,
+    autoplay: false
+});
---
+++ typescript/day01.ts
@@ -0,0 +1,18 @@
+class Student {
+  fullName: string;
+  constructor(public firstName, public middleInitial, public lastName) {
+    this.fullName = firstName + " " + middleInitial + " " + lastName;
+  }
+}
+
+interface Person{
+  firstName: string;
+  lastName: string;
+}
+
+function greeter(person: Person) {
+  return `Hello, ${person.firstName} ${person.lastName}`;
+}
+
+const user = new Student("Jane", "M.", "User");
+console.log(greeter(user));
