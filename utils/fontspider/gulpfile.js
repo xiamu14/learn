99649054c869a6c2093505324701e8d191bbfedd
xiamu <544652471@qq.com>
2018-04-22 23:32:03 +0800

    feat: 删掉不必要的文件，保持项目的干净

--- utils/fontspider/gulpfile.js
+++ utils/fontspider/gulpfile.js
@@ -1,9 +1,10 @@
 var gulp = require('gulp');
-var fontSpider = require('gulp-font-spider');
+var fontmin = require('gulp-fontmin');
 
-gulp.task('fontspider', function () {
-  return gulp.src('./index.html')
-    .pipe(fontSpider());
+gulp.task('default', function () {
+  return gulp.src('./font/custom.ttf')
+    .pipe(fontmin({
+      text: '猜歌段位赛好友对战音符银行排行榜道具中心抢答赛+0123456789',
+    }))
+    .pipe(gulp.dest('dist/fonts'));
 });
-
-gulp.task('default', ['fontspider']);
