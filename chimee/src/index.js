c79d4b1e4a57c7bcbc8678ca3adc1c60db82e79e
xiamu <544652471@qq.com>
2018-08-03 18:51:21 +0800

    feat: 保存测试代码

--- chimee/src/index.js
+++ chimee/src/index.js
@@ -1,8 +1,19 @@
-import ChimeePlayer from 'chimee-player';
+import ChimeeMobilePlayer from 'chimee-mobile-player';
+import bowser from 'bowser';
+import 'chimee-mobile-player/lib/chimee-mobile-player.browser.css';
 
-const chimee = new ChimeePlayer({
-    wrapper: '#wrapper',
+const video = document.getElementsByClassName('video');
+for(let element of video ){
+  new ChimeeMobilePlayer({
+    wrapper: element,
     src: 'http://cdn.toxicjohann.com/lostStar.mp4',
     controls: true,
-    autoplay: false
-});
+    autoplay: false,
+    playsInline: true,
+    poster: 'http://img3.imgtn.bdimg.com/it/u=1178439972,3652791397&fm=27&gp=0.jpg',
+  });
+};
+
+const browser = bowser.getParser(window.navigator.userAgent);
+impression.userTechData = browser.parse();
+console.log(impression.userTechData);
