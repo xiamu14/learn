d1666f2229364d9a674a603cbbed060e3165f849
xiamu <544652471@qq.com>
2018-08-27 15:06:19 +0800

    feat: 验证vue chimee 没有问题应该

--- chimee/src/index.js
+++ chimee/src/index.js
@@ -1,19 +1,8 @@
-import ChimeeMobilePlayer from 'chimee-mobile-player';
-import 'chimee-mobile-player/lib/chimee-mobile-player.browser.css';
+import Vue from 'vue';
+import Video from './video.vue';
 
-const video = document.getElementsByClassName('video')[0];
-const header = document.querySelector('header');
-header.addEventListener('click', () => {
-  const player = new ChimeeMobilePlayer({
-    wrapper: '.video',
-    src: 'http://cdn.toxicjohann.com/lostStar.mp4',
-    box: 'native',
-    controls: true,
-    autoplay: false,
-    playsInline: true,
-    poster: 'http://img3.imgtn.bdimg.com/it/u=1178439972,3652791397&fm=27&gp=0.jpg',
-    preload: 'auto',
-  });
-  player.play();
-  console.log(document.querySelector('video'));
-});
+new Vue({
+  render(h) {
+    return h(Video);
+  }
+}).$mount('#app');
