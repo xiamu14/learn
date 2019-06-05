a4992bab90923ed9524e0a3b97d27cadfe30ffad
xiamu <544652471@qq.com>
2019-06-05 09:21:57 +0800

    feat: uglify binary_search_tree

--- data_stucture/binary_search_tree.ts
+++ data_stucture/binary_search_tree.ts
@@ -1,5 +1,20 @@
 interface IKey {
-  key: number
+  key: number;
+}
+
+enum Eorder {
+  /**
+   * @desc 中序遍历
+   */
+  in,
+  /**
+   * @desc 先序遍历
+   */
+  pre,
+  /**
+   * @desc 后序遍历
+   */
+  post
 }
 
 class TreeNode<T extends IKey> {
@@ -7,7 +22,7 @@ class TreeNode<T extends IKey> {
   right: null | TreeNode<T>;
   key: number;
   entity: T;
-  constructor(entity: T ) {
+  constructor(entity: T) {
     this.key = entity.key;
     this.entity = entity;
     this.left = null;
@@ -32,7 +47,6 @@ class BinarySearchTree<T extends IKey> {
       this.insertNode(this.root, newNode);
     }
   }
-
   private insertNode(node: TreeNode<T>, newNode: TreeNode<T>) {
     if (newNode.key < node.key) {
       if (node.left === null) {
@@ -46,6 +60,53 @@ class BinarySearchTree<T extends IKey> {
       } else {
         this.insertNode(node.right, newNode);
       }
+    }
+  }
+
+  /**
+   * @desc 遍历，包含中序，先序，后序
+   */
+  orderTraverse(type: Eorder, callback: (param: TreeNode<T>) => void) {
+    if (this.root) {
+      this.orderTraverseNode(type, this.root, callback);
+    }
+  }
+
+  private orderTraverseNode(
+    type: Eorder,
+    node: TreeNode<T>,
+    callback: (param: TreeNode<T>) => void
+  ) {
+    switch (type) {
+      case Eorder.in:
+        if (node.left) {
+          this.orderTraverseNode(type, node.left, callback);
+        }
+        callback(node);
+        if (node.right) {
+          this.orderTraverseNode(type, node.right, callback);
+        }
+        break;
+      case Eorder.pre:
+        callback(node);
+        if (node.left) {
+          this.orderTraverseNode(type, node.left, callback);
+        }
+        if (node.right) {
+          this.orderTraverseNode(type, node.right, callback);
+        }
+        break;
+      case Eorder.post:
+        if (node.left) {
+          this.orderTraverseNode(type, node.left, callback);
+        }
+        if (node.right) {
+          this.orderTraverseNode(type, node.right, callback);
+        }
+        callback(node);
+        break;
+      default:
+        break;
     }
   }
 }
@@ -66,14 +127,26 @@ type fruit = {
    * @desc 用于二叉搜索树时的排序值，值等于 sweetness
    */
   key: number;
+};
 
-}
+const fruitSweetness = new BinarySearchTree<fruit>();
 
-const fruitSweetness = new BinarySearchTree<fruit>()
+fruitSweetness.insert({ name: "西瓜", sweetness: 100, key: 100 });
+fruitSweetness.insert({ name: "草莓", sweetness: 80, key: 80 });
+fruitSweetness.insert({ name: "苹果", sweetness: 10, key: 10 });
 
-fruitSweetness.insert({name: '西瓜', sweetness: 100, key: 100});
-fruitSweetness.insert({name: '苹果', sweetness: 10, key: 10});
+fruitSweetness.orderTraverse(Eorder.in, (fruit: TreeNode<fruit>) => {
+  console.log(fruit.entity.name);
+});
 
-if (fruitSweetness.root) {
-  console.log(fruitSweetness.root);
-}
+console.log("-------- /n");
+
+fruitSweetness.orderTraverse(Eorder.pre, (fruit: TreeNode<fruit>) => {
+  console.log(fruit.entity.name);
+});
+
+console.log("-------- /n");
+
+fruitSweetness.orderTraverse(Eorder.post, (fruit: TreeNode<fruit>) => {
+  console.log(fruit.entity.name);
+});
