13957c98e91123291559438033088d99b6678251
ben <544652471@qq.com>
2020-03-24 09:57:40 +0800

    feat: add some feature.

---
+++ learnflutter/lib/main.dart
@@ -0,0 +1,76 @@
+import 'package:flutter/material.dart';
+
+void main() => runApp(MyApp());
+
+class MyApp extends StatelessWidget {
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        // This is the theme of your application.
+        //
+        // Try running your application with "flutter run". You'll see the
+        // application has a blue toolbar. Then, without quitting the app, try
+        // changing the primarySwatch below to Colors.green and then invoke
+        // "hot reload" (press "r" in the console where you ran "flutter run",
+        // or simply save your changes to "hot reload" in a Flutter IDE).
+        // Notice that the counter didn't reset back to zero; the application
+        // is not restarted.
+        primarySwatch: Colors.blue,
+      ),
+      home: MyHomePage(title: 'Flutter Demo Home Page'),
+    );
+  }
+}
+
+class MyHomePage extends StatelessWidget {
+  final String title;
+
+  MyHomePage({this.title});
+
+  @override
+  Widget build(BuildContext context) {
+    return DefaultTabController(
+      length: 3,
+      child: Scaffold(
+          backgroundColor: Colors.grey[100],
+          appBar: AppBar(
+            leading: IconButton(
+              icon: Icon(Icons.menu),
+              tooltip: "Navigration",
+              onPressed: () => debugPrint('Navigration button is pressed'),
+            ),
+            title: Text(title),
+            actions: <Widget>[
+              IconButton(
+                icon: Icon(Icons.search),
+                tooltip: "Search",
+                onPressed: () => debugPrint('Search button is pressed'),
+              )
+            ],
+            elevation: 0.0,
+            bottom: TabBar(
+              unselectedLabelColor: Colors.black38,
+              tabs: <Widget>[
+                Tab(
+                  icon: Icon(Icons.local_florist),
+                ),
+                Tab(
+                  icon: Icon(Icons.change_history),
+                ),
+                Tab(
+                  icon: Icon(Icons.directions_bike),
+                ),
+              ],
+            ),
+          ),
+          body: TabBarView(children: <Widget>[
+            Icon(Icons.local_florist, size: 128.0, color: Colors.black12),
+            Icon(Icons.change_history, size: 128.0, color: Colors.black12),
+            Icon(Icons.directions_bike, size: 128.0, color: Colors.black12),
+          ])),
+    );
+  }
+}
