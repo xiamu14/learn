c16b79bea605e10cb312d8241cb36bcafdb2d3e8
ben <544652471@qq.com>
2020-05-30 23:18:42 +0800

    feat: remove some useless project.

---
+++ learn-python/app.py
@@ -0,0 +1,13 @@
+# -*- coding: utf-8 -*-
+
+# import tensorflow as tf
+
+# # Set up a linear classifier.
+# classifier = tf.estimator.LinearClassifier()
+
+# # Train the model on some example data.
+# classifier.train(input_fn=train_input_fn, steps=2000)
+
+# # Use it to predict.
+# predictions = classifier.predict(input_fn=predict_input_fn)
+w
