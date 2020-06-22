663d7c9a7b6541585aa9b1c23ac03dc70e9ca2f3
ben <544652471@qq.com>
2020-06-22 10:08:56 +0800

    fix: fix deno version error.

--- learn-deno/app.ts
+++ learn-deno/app.ts
@@ -1,8 +1,25 @@
-import { serve } from "https://deno.land/std/http/server.ts";
+import { Application } from "https://deno.land/x/oak/mod.ts";
 
-const server = serve({ port: 8000 });
+const app = new Application();
 
-const greeting:string = "I'm a Deno server, you found me!";
-for await (const req of server) {
-    req.respond({ body: greeting});
-}
+// Logger
+app.use(async (ctx:any, next:any) => {
+  await next();
+  const rt = ctx.response.headers.get("X-Response-Time");
+  console.log(`${ctx.request.method} ${ctx.request.url} - ${rt}`);
+});
+
+// Timing
+app.use(async (ctx:any, next:any) => {
+  const start = Date.now();
+  await next();
+  const ms = Date.now() - start;
+  ctx.response.headers.set("X-Response-Time", `${ms}ms`);
+});
+
+// Hello World!
+app.use((ctx:any) => {
+  ctx.response.body = "Hello World!";
+});
+
+await app.listen({ port: 8000 });
