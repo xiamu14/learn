e129342160cfff5879639a5c4ed34ce6bab1c0f2
ben <544652471@qq.com>
2020-08-31 10:51:54 +0800

    feat: add some feature.

--- prisma-next-nexus/components/AllUsers.tsx
+++ prisma-next-nexus/components/AllUsers.tsx
@@ -1,27 +1,8 @@
 import React from "react";
-import gql from "graphql-tag";
-import { useQuery } from "urql";
-
-const AllUserSQuery = gql`
-  query {
-    allUsers {
-      id
-      name
-    }
-  }
-`;
-
-type AllUsersData = {
-  allUsers: {
-    id: string;
-    name: string;
-  }[];
-};
+import { useAllUsersQuery } from "../generated/graphql";
 
 export default function AllUsers() {
-  const [result] = useQuery<AllUsersData>({
-    query: AllUserSQuery,
-  });
+  const [result] = useAllUsersQuery();
   const { data, fetching, error } = result;
 
   if (fetching) return <p>Loading...</p>;
@@ -29,9 +10,9 @@ export default function AllUsers() {
 
   return (
     <div>
-      <p>There are {data?.allUsers.length} user(s) in the database:</p>
+      <p>There are {data?.allUsers?.length} user(s) in the database:</p>
       <ul>
-        {data?.allUsers.map((user) => (
+        {data?.allUsers?.map((user) => (
           <li key={user.id}>{user.name}</li>
         ))}
       </ul>
---
+++ prisma-next-nexus/generated/graphql.tsx
@@ -0,0 +1,161 @@
+import gql from 'graphql-tag';
+import * as Urql from 'urql';
+export type Maybe<T> = T | null;
+export type Exact<T extends { [key: string]: unknown }> = { [K in keyof T]: T[K] };
+export type Omit<T, K extends keyof T> = Pick<T, Exclude<keyof T, K>>;
+/** All built-in and custom scalars, mapped to their actual values */
+export type Scalars = {
+  ID: string;
+  String: string;
+  Boolean: boolean;
+  Int: number;
+  Float: number;
+  /** A date-time string at UTC, such as 2007-12-03T10:15:30Z, compliant with the `date-time` format outlined in section 5.6 of the RFC 3339 profile of the ISO 8601 standard for representation of dates and times using the Gregorian calendar. */
+  DateTime: any;
+  /** The `JSON` scalar type represents JSON objects as specified by [ECMA-404](http://www.ecma-international.org/publications/files/ECMA-ST/ECMA-404.pdf). */
+  Json: any;
+};
+
+export type BatchPayload = {
+  __typename?: 'BatchPayload';
+  count: Scalars['Int'];
+};
+
+
+
+export type Mutation = {
+  __typename?: 'Mutation';
+  bigRedButton?: Maybe<Scalars['String']>;
+  createOneUser: User;
+  deleteOneUser?: Maybe<User>;
+  updateOneUser?: Maybe<User>;
+  updateManyUser: BatchPayload;
+};
+
+
+export type MutationCreateOneUserArgs = {
+  data: UserCreateInput;
+};
+
+
+export type MutationDeleteOneUserArgs = {
+  where: UserWhereUniqueInput;
+};
+
+
+export type MutationUpdateOneUserArgs = {
+  data: UserUpdateInput;
+  where: UserWhereUniqueInput;
+};
+
+
+export type MutationUpdateManyUserArgs = {
+  data: UserUpdateManyMutationInput;
+  where?: Maybe<UserWhereInput>;
+};
+
+export type NestedStringFilter = {
+  equals?: Maybe<Scalars['String']>;
+  in?: Maybe<Array<Scalars['String']>>;
+  notIn?: Maybe<Array<Scalars['String']>>;
+  lt?: Maybe<Scalars['String']>;
+  lte?: Maybe<Scalars['String']>;
+  gt?: Maybe<Scalars['String']>;
+  gte?: Maybe<Scalars['String']>;
+  contains?: Maybe<Scalars['String']>;
+  startsWith?: Maybe<Scalars['String']>;
+  endsWith?: Maybe<Scalars['String']>;
+  not?: Maybe<NestedStringFilter>;
+};
+
+export type Query = {
+  __typename?: 'Query';
+  allUsers?: Maybe<Array<User>>;
+  user?: Maybe<User>;
+  users: Array<User>;
+};
+
+
+export type QueryUserArgs = {
+  where: UserWhereUniqueInput;
+};
+
+
+export type QueryUsersArgs = {
+  first?: Maybe<Scalars['Int']>;
+  last?: Maybe<Scalars['Int']>;
+  before?: Maybe<UserWhereUniqueInput>;
+  after?: Maybe<UserWhereUniqueInput>;
+};
+
+export type StringFilter = {
+  equals?: Maybe<Scalars['String']>;
+  in?: Maybe<Array<Scalars['String']>>;
+  notIn?: Maybe<Array<Scalars['String']>>;
+  lt?: Maybe<Scalars['String']>;
+  lte?: Maybe<Scalars['String']>;
+  gt?: Maybe<Scalars['String']>;
+  gte?: Maybe<Scalars['String']>;
+  contains?: Maybe<Scalars['String']>;
+  startsWith?: Maybe<Scalars['String']>;
+  endsWith?: Maybe<Scalars['String']>;
+  not?: Maybe<NestedStringFilter>;
+};
+
+export type User = {
+  __typename?: 'User';
+  id: Scalars['String'];
+  name: Scalars['String'];
+};
+
+export type UserCreateInput = {
+  id?: Maybe<Scalars['String']>;
+  name: Scalars['String'];
+};
+
+export type UserUpdateInput = {
+  id?: Maybe<Scalars['String']>;
+  name?: Maybe<Scalars['String']>;
+};
+
+export type UserUpdateManyMutationInput = {
+  id?: Maybe<Scalars['String']>;
+  name?: Maybe<Scalars['String']>;
+};
+
+export type UserWhereInput = {
+  AND?: Maybe<Array<UserWhereInput>>;
+  OR?: Maybe<Array<UserWhereInput>>;
+  NOT?: Maybe<Array<UserWhereInput>>;
+  id?: Maybe<StringFilter>;
+  name?: Maybe<StringFilter>;
+};
+
+export type UserWhereUniqueInput = {
+  id?: Maybe<Scalars['String']>;
+};
+
+export type AllUsersQueryVariables = Exact<{ [key: string]: never; }>;
+
+
+export type AllUsersQuery = (
+  { __typename?: 'Query' }
+  & { allUsers?: Maybe<Array<(
+    { __typename?: 'User' }
+    & Pick<User, 'id' | 'name'>
+  )>> }
+);
+
+
+export const AllUsersDocument = gql`
+    query AllUsers {
+  allUsers {
+    id
+    name
+  }
+}
+    `;
+
+export function useAllUsersQuery(options: Omit<Urql.UseQueryArgs<AllUsersQueryVariables>, 'query'> = {}) {
+  return Urql.useQuery<AllUsersQuery>({ query: AllUsersDocument, ...options });
+};
