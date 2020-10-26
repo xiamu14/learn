1fe6133a7e6330ef0b43948c373b8a92263cc325
ben <544652471@qq.com>
2020-10-26 15:32:25 +0800

    fix: fix some bug.

--- smooth-scrollbar/src/App.tsx
+++ smooth-scrollbar/src/App.tsx
@@ -13,33 +13,38 @@ function App() {
   ) => {
     if (scrollValues.scrollTop !== prevScrollValues.scrollTop) {
       setIsScrollY(true);
-      setIsScrollX(false);
+      if (scrollValues.scrollLeft === 0) {
+        setIsScrollX(false);
+      }
     }
     if (scrollValues.scrollLeft !== prevScrollValues.scrollLeft) {
       setIsScrollX(true);
-      setIsScrollY(false);
+      if (scrollValues.scrollTop === 0) {
+        setIsScrollY(false);
+      }
     }
   };
   return (
     <div className="App">
-      <Scrollbar
-        className="content"
-        onScroll={handleScroll as any}
-      >
-        <div className="header">header</div>
-        <div className="sidebar">sidebar</div>
+      <Scrollbar className="content" onScroll={handleScroll as any}>
+        <div className="header">
+          <div className="common_area"></div>
+        </div>
+        <div className="sidebar">
+          <div className="common_area"></div>
+        </div>
         <div className="data">
           datadatadatadatadatadatadatadatadatadatadatadatadatadatadatadatadatadatadatadatadatadatadatadatadatadatadatadatadatadatadatadatadatadatadatadatadatadatadatadatadatadatadatadatadatadatadatadatadatadatadatadatadatadatadata
         </div>
       </Scrollbar>
       {isScrollY ? (
-        <div className="header" style={{ top: "10vh", left: "10vw" }}>
-          header
+        <div className="header" style={{ top: "0", left: "0", zIndex: 102 }}>
+          <div className="common_area"></div>
         </div>
       ) : null}
       {isScrollX ? (
-        <div className="sidebar" style={{ top: "10vh", left: "10vw" }}>
-          sidebar
+        <div className="sidebar" style={{ top: "0", left: "0", zIndex: 102 }}>
+          <div className="common_area"></div>
         </div>
       ) : null}
     </div>
