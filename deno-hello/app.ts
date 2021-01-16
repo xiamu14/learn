4210357bca40b378c12a81bf9591dd3cbae2f5a4
ben <544652471@qq.com>
2021-01-17 01:44:37 +0800

    feat: add deno-hello.

---
+++ deno-hello/app.ts
@@ -0,0 +1,9 @@
+import { serve } from "https://deno.land/std@0.83.0/http/server.ts";
+
+const s = serve({ port: 8000 });
+
+console.log("http://localhost:8000/");
+
+for await (const req of s) {
+  req.respond({ body: "Hello World\n" });
+}
