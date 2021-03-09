816087eb8ce020314f83ea6182f08abfa33a5dbe
ben <544652471@qq.com>
2021-03-09 09:14:33 +0800

    feat: new project.

---
+++ try-vitejs-react/src/stories/Header.stories.tsx
@@ -0,0 +1,20 @@
+import React from 'react';
+// also exported from '@storybook/react' if you can deal with breaking changes in 6.1
+import { Story } from '@storybook/react';
+
+import { Header, HeaderProps } from './Header';
+
+export default {
+  title: 'Example/Header',
+  component: Header,
+};
+
+const Template: Story<HeaderProps> = (args) => <Header {...args} />;
+
+export const LoggedIn = Template.bind({});
+LoggedIn.args = {
+  user: {},
+};
+
+export const LoggedOut = Template.bind({});
+LoggedOut.args = {};
