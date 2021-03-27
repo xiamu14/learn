ae24bc711dcfa9a9002c37817dfa35c3956edbd4
ben <544652471@qq.com>
2021-03-27 09:35:06 +0800

    feat: add some rust.

--- learn-cargo/src/main.rs
+++ learn-cargo/src/main.rs
@@ -1,17 +1,25 @@
-struct Rectangle {
-  width: u32,
-  height: u32,
-}
+use rand::Rng;
+use std::cmp::Ordering;
+use std::io;
 
 fn main() {
-  let rect1 = Rectangle { width: 30, height: 50 };
+  println!("猜数字\n输入数字");
 
-  println!(
-      "The area of the rectangle is {} square pixels.",
-      area(&rect1)
-  );
-}
+  let mut guess = String::new();
 
-fn area(rectangle: &Rectangle) -> u32 {
-  rectangle.width * rectangle.height
+  let secret_number = rand::thread_rng().gen_range(0, 100);
+
+  println!("神秘数字：{}", secret_number);
+
+  io::stdin().read_line(&mut guess).expect("无法获取输入的数");
+
+  println!("你猜测的数是：{}", guess);
+
+  let guess: u32 = guess.trim().parse().expect("请输入数字");
+
+  match guess.cmp(&secret_number) {
+    Ordering::Less => println!("数字太小了"),
+    Ordering::Greater => println!("数字太大了"),
+    Ordering::Equal => println!("猜对了"),
+  }
 }
