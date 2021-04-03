f3f45b7420df6ce9d16b3e0cef27c0e785648fa5
ben <544652471@qq.com>
2021-04-03 23:09:17 +0800

    feat: remove some files.

--- learn-cargo/src/main.rs
+++ learn-cargo/src/main.rs
@@ -1,25 +1,8 @@
-use rand::Rng;
-use std::cmp::Ordering;
-use std::io;
+
 
 fn main() {
-  println!("猜数字\n输入数字");
+  let action = std::env::args().nth(1).expect("Please specify an action");
+  let item = std::env::args().nth(2).expect("Please specify an item");
 
-  let mut guess = String::new();
-
-  let secret_number = rand::thread_rng().gen_range(0, 100);
-
-  println!("神秘数字：{}", secret_number);
-
-  io::stdin().read_line(&mut guess).expect("无法获取输入的数");
-
-  println!("你猜测的数是：{}", guess);
-
-  let guess: u32 = guess.trim().parse().expect("请输入数字");
-
-  match guess.cmp(&secret_number) {
-    Ordering::Less => println!("数字太小了"),
-    Ordering::Greater => println!("数字太大了"),
-    Ordering::Equal => println!("猜对了"),
-  }
+  println!("{:?}, {:?}", action, item);
 }
--- learn-go-wasm-node/entity/video.go
+++
@@ -1,8 +0,0 @@
-package entity
-
-// Video entity
-type Video struct {
-	Title       string `json:"title"`
-	Description string `json:"description"`
-	URL         string `json:"url"`
-}
